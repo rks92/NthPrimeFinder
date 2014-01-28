96b3ce7bde269f29c409e0b26444d87e54e929d1
rks92 <rahul.shardha@gmail.com>
2014-01-28 14:32:28 -0500

    Initial & Working Commit
    Initial & Working Commit

---
+++ src/com/primefinder/main/Main.java
@@ -0,0 +1,35 @@
+package com.primefinder.main;
+
+import java.util.Scanner;
+import java.util.Vector;
+
+public class Main {
+	public static void main (String[] args){
+		
+		Scanner stdin = new Scanner(System.in);
+		String NthPrime = stdin.nextLine();
+		stdin.close();
+		
+		long NthPrimeToFind = Long.parseLong(NthPrime);
+		
+		Vector<Long> primes = new Vector<Long>();
+		primes.add((long) 2);
+		
+		boolean isPrime = true;
+		long i = 3;
+		
+		while (primes.size() < NthPrimeToFind){
+			for (int j = 0; j < primes.size(); j++){
+				if (i % primes.elementAt(j) == 0)
+					isPrime = false;
+			}
+			if (isPrime == true){
+				primes.add(i);
+			}
+			isPrime = true;
+			i++;
+		}
+		
+		System.out.println(primes.lastElement());
+	}
+}
